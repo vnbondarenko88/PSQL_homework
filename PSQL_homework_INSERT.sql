9ddc9509bcc480dc02ace3429f0ead52b592599f
Vadim Bondarenko <vdm88@mail.ru>
2024-05-08 18:03:52 +0700

    finish

--- PSQL_homework_INSERT.sql
+++ PSQL_homework_INSERT.sql
@@ -5,52 +5,78 @@ VALUES  ('Хип-хоп'),
 
 
 INSERT INTO singer (name)
-VALUES 	('Eminem'), 
-		('50 cent'),
-		('БИ-2'),
-		('Кино'),
-		('Дмитрий Билан'),
-		('30 seconds to mars');
+VALUES ('Eminem'), 
+	  ('50 cent'),
+	  ('БИ-2'),
+	  ('Кино'),
+	  ('Дмитрий Билан'),
+	  ('30 seconds to mars'),
+	  ('singer_1'),
+	  ('singer_2'),
+	  ('singer_3');
 
 
 
 INSERT INTO musical_genre_singer (musical_genre_id, singer_id)
-VALUES	(1, 1),
-		(1, 2),
-		(2, 3),
-		(2, 4),
-		(3, 5),
-		(2, 6);
+VALUES (1, 1),
+	   (1, 2),
+	   (2, 3),
+	   (2, 4),
+	   (3, 5),
+	   (2, 6),
+	   (3, 7),
+	   (1, 8),
+	   (2, 9);
 
 
 INSERT INTO album (album_name, year_release)
-VALUES	('Recovery', 2010),
-		('The Massacre', 2005),
-		('Аллилуя', 2022),
-		('Звезда по имени солнце', 1989),
-		('Believe', 2009),
-		('America', 2018);
+VALUES ('Recovery', 2010),
+	   ('The Massacre', 2005),
+	   ('Аллилуя', 2022),
+	   ('Звезда по имени солнце', 1989),
+	   ('Believe', 2009),
+	   ('America', 2019),
+	   ('album_1', 2020),
+	   ('album_2', 2021),
+	   ('album_3', 2018);
 
 
 
 INSERT INTO singer_album (album_id, singer_id)
-VALUES	(1, 1),
-		(2, 2),
-		(3, 3),
-		(4, 4),
-		(5, 5),
-		(6, 6);
+VALUES (1, 1),
+	   (2, 2),
+	   (3, 3),
+	   (4, 4),
+	   (5, 5),
+	   (6, 6),
+	   (7, 7),
+	   (8, 8),
+	   (9, 9),
+	   (9, 2),
+	   (9, 3),
+	   (7, 1);
 
 
 
 
 INSERT INTO track (track_name, track_duration, album_id)
-VALUES	('So Bad', 325, 1),
-		('Get in my car', 245, 2),
-		('Депрессия', 251, 3),
-		('Звезда по имени солнце', 225, 4),
-		('Mistakes', 230, 5),
-		('«Walk on water', 185, 6);
+VALUES ('So Bad', 325, 1),
+       ('Get in my car', 245, 2),
+       ('Депрессия', 251, 3),
+       ('Звезда по имени солнце', 225, 4),
+       ('Mistakes', 230, 5),
+       ('«Walk on water', 185, 6),
+       ('track_1', 210, 7),
+       ('track_2', 250, 8),
+       ('track_3', 190, 9),
+       ('track_4', 210, 1),
+       ('track_5', 230, 2),
+       ('track_6', 213, 3),
+       ('track_7', 198, 4),
+       ('track_8', 215, 5),
+       ('track_9', 220, 6),
+       ('track_10', 220, 7),
+       ('track_11', 220, 9);
 
 
 
@@ -67,18 +93,19 @@ VALUES	('first_collection', 2019),
 
 
 INSERT INTO track_collection (track_id, collection_id)
-VALUES	(4, 1),
-		(5, 1),
-		(2, 1),
-		(1, 2),
-		(3, 2),
-		(6, 2),
-		(3, 3),
-		(4, 3),
-		(5, 3),
-		(1, 4),
-		(2, 4),
-		(6, 4);
+VALUES (4, 1),
+	   (5, 1),
+	   (2, 1),
+	   (1, 2),
+	   (3, 2),
+	   (6, 2),
+	   (3, 3),
+	   (4, 3),
+	   (5, 3),
+	   (1, 4),
+	   (2, 4),
+	   (6, 4),
+	   (7, 1);
 
 
 
--- PSQL_homework_SELECT.sql
+++ PSQL_homework_SELECT.sql
@@ -1,28 +1,32 @@
 -- Задание 2 
 
--- 2.1
+-- 2.1 Название и продолжительность самого длительного трека.
 SELECT track_name, MAX(track_duration)
 FROM track
 GROUP BY track_name
 ORDER BY MAX(track_duration) DESC
 LIMIT 1;
 
--- 2.2
+
+-- 2.2 Название треков, продолжительность которых не менее 3,5 минут.
 SELECT track_name, track_duration
 FROM track
 WHERE track_duration >= 210;
 
--- 2.3
+
+-- 2.3 Названия сборников, вышедших в период с 2018 по 2020 год включительно.
 SELECT name
 FROM collection
 WHERE year_release BETWEEN 2018 AND 2020;
 
--- 2.4
+
+-- 2.4 Исполнители, чьё имя состоит из одного слова.
 SELECT name
 FROM singer
 WHERE name NOT LIKE '% %';
 
--- 2.5
+
+-- 2.5 Название треков, которые содержат слово «мой» или «my».
 SELECT track_name 
 FROM track
 WHERE track_name LIKE '% my %' OR track_name LIKE '% мой %';
@@ -30,7 +34,7 @@ WHERE track_name LIKE '% my %' OR track_name LIKE '% мой %';
 
 -- Задание 3 
 
--- 3.1
+-- 3.1 Количество исполнителей в каждом жанре.
 SELECT mg.name, COUNT(s.singer_id) 
 FROM musical_genre mg 
 	JOIN musical_genre_singer mgs
@@ -39,22 +43,24 @@ FROM musical_genre mg
 	ON mgs.singer_id = s.singer_id 
 GROUP BY mg.name; 
 
--- 3.2
 
+-- 3.2 Количество треков, вошедших в альбомы 2019–2020 годов.
 SELECT COUNT(*)
 FROM track t
 	JOIN album a 
 	ON t.album_id = a.album_id 
 WHERE year_release BETWEEN 2019 AND 2020;
 
--- 3.3
+
+-- 3.3 Средняя продолжительность треков по каждому альбому.
 SELECT album_name, AVG(track_duration)
 FROM album a
 	JOIN track t
 	ON a.album_id  = t.album_id  
 GROUP BY album_name; 
 
--- 3.4
+
+-- 3.4 Все исполнители, которые не выпустили альбомы в 2020 году.
 SELECT name
 FROM singer s 
 	JOIN singer_album sa 
@@ -63,7 +69,8 @@ FROM singer s
 	ON sa.album_id = a.album_id 
 WHERE year_release != 2020;
 
--- 3.5
+
+-- 3.5 Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 SELECT c.name 
 FROM singer s
 	JOIN singer_album sa 
@@ -77,3 +84,60 @@ FROM singer s
 	JOIN collection c 
 	ON tc.collection_id = c.collection_id 
 WHERE s.name LIKE 'Eminem';
+
+
+
+-- Задание 4
+
+-- 4.1 Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
+SELECT c.name 
+FROM musical_genre mg 
+	JOIN musical_genre_singer mgs
+	ON mg.musical_genre_id = mgs.musical_genre_id 
+	JOIN singer s 
+	ON mgs.singer_id = s.singer_id 
+	JOIN singer_album sa 
+	ON s.singer_id = sa.singer_id 
+	JOIN album a
+	ON sa.album_id = a.album_id 
+	JOIN track t
+	ON a.album_id = t.album_id 
+	JOIN track_collection tc 
+	ON t.track_id  = tc.track_id 
+	JOIN collection c 
+	ON tc.collection_id = c.collection_id
+GROUP BY c.name
+HAVING COUNT(s.singer_id) > 1;
+
+
+-- 4.2 Наименования треков, которые не входят в сборники.
+SELECT track_name 
+FROM track t 
+	LEFT JOIN track_collection tc 
+	ON t.track_id = tc.track_id 
+WHERE tc.track_id IS NULL;
+
+
+-- 4.3 Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, 
+-- теоретически таких треков может быть несколько.
+SELECT s.name
+FROM singer s 
+	JOIN singer_album sa 
+	ON s.singer_id = sa.singer_id 
+	JOIN album a 
+	ON sa.album_id = a.album_id 
+	JOIN track t 
+	ON a.album_id = t.album_id
+WHERE t.track_duration <= (SELECT MIN(track_duration) FROM track);
+
+
+-- 4.4 Названия альбомов, содержащих наименьшее количество треков.
+SELECT a.album_name, COUNT(t.track_id)
+FROM album a 
+	JOIN track t
+	ON a.album_id = t.album_id 
+GROUP BY a.album_name
+ORDER BY COUNT(t.track_id) 
+LIMIT 1;
+
+
